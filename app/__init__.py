93f706d225a6ee1a913e2d035d8755fa1ce5258b
Gustavo Farias de Araujo <bug.ufrj@gmail.com>
2024-09-01 10:15:16 -0300

    incluindo parte de banco de dados

--- app/__init__.py
+++ app/__init__.py
@@ -1,8 +1,10 @@
 # import flast module
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
 
 # instance of flask application
 app = Flask(__name__)
-
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////tmp/test.db'
+db = SQLAlchemy(app)
 
 from app.controllers import payments
