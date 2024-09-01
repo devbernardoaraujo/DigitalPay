1caed2e3e7e6da3d7b0df573ebbd60116b03592c
Gustavo Farias de Araujo <bug.ufrj@gmail.com>
2024-09-01 20:11:26 -0300

    criando arquivo de confiração local

--- app/__init__.py
+++ app/__init__.py
@@ -1,10 +1,11 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
+from app import config_local
 
 # instance of flask application
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:720horasgratis@localhost/inovafinance'
+app.config['SQLALCHEMY_DATABASE_URI'] = config_local.SQLALCHEMY_DATABASE_URI
 db = SQLAlchemy(app)
 
 migrate = Migrate(app, db)
