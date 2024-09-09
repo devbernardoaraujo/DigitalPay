04055e0afe91252c2c3f9aaf010f0b4dd3d20873
devbernardoaraujo <bernardobaraujo22@gmail.com>
2024-09-08 22:12:34 -0300

    autenticação utilizando o bcrypt

--- app/models/payments.py
+++ app/models/payments.py
@@ -1,7 +1,6 @@
-from app import app, db
+from app import app, db, bcrypt
 
-class Marketplaces(db.Model):
-
+class Marketplace(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     public_key = db.Column(db.String(80), unique=True, nullable=False)
     private_key = db.Column(db.String(80), unique=True, nullable=False)
@@ -12,13 +11,21 @@ class Marketplaces(db.Model):
     def __repr__(self):
         return '<Marketplace %r>' % self.name
 
-# class User(db.Model):
-#
-#     id = db.Column(db.Integer, primary_key=True)
-#     name = db.Column(db.String(80), nullable=False)
-#     password = db.Column(db.String(80), nullable=False)
-#     login = db.Column(db.String(80), unique=True, nullable=False)
-#     marketplace_id = db.Column(db.Integer, db.foreignKey('marketplace.id'))
-#
-#     def __repr__(self):
-#         return '<User %r>' % self.name
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100), nullable=False)
+    password = db.Column(db.String(80), nullable=False)
+    login = db.Column(db.String(80), unique=True, nullable=False)
+    marketplace_id = db.Column(db.Integer, db.ForeignKey('marketplace.id'))
+
+    def __init__(self, login, password, name):
+        self.name = name
+        self.login = login
+        self.password = bcrypt.generate_password_hash(password).decode('utf-8')
+
+    def check_password(self, password):
+        return bcrypt.check_password_hash(self.password, password.encode('utf-8'))
+
+    def __repr__(self):
+        return '<User %r>' % self.name
+
