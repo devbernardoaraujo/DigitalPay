0b4390b77088721ca639bbaa8bcec1d016a99d4a
devbernardoaraujo <bernardobaraujo22@gmail.com>
2024-10-14 17:59:51 -0300

    integracao com a api da zoop, e formulario da rota PF

--- app/controllers/payments.py
+++ app/controllers/payments.py
@@ -64,3 +64,72 @@ def transfers():
     transfers_list = res.json().get('items', [])
 
     return render_template('transfers.html', transfers_list=transfers_list)
+
+
+@app.route("/pessoafisica", methods=["GET", "POST"])
+@login_required
+def pessoa_fisica():
+    if request.method == "POST":
+        # obter dados do user no formulario
+        first_name = request.form.get("first_name")
+        last_name = request.form.get("last_name")
+        email = request.form.get("email")
+        phone_number = request.form.get("phone_number")
+        taxpayer_id = request.form.get("taxpayer_id")
+        birthdate = request.form.get("birthdate")
+        statement_descriptor = request.form.get("statement_descriptor")
+        revenue = request.form.get("revenue")
+
+        address = {
+            "line1": request.form.get("line1"),
+            "line2": request.form.get("line2"),
+            "line3": request.form.get("line3"),
+            "neighborhood": request.form.get("neighborhood"),
+            "city": request.form.get("city"),
+            "state": request.form.get("state"),
+            "postal_code": request.form.get("postal_code"),
+            "country_code": request.form.get("country_code"),
+        }
+
+        mcc = request.form.get("mcc")
+
+        # processar dados enviados pelo user e chamar api da zoop
+        marketplace = Marketplaces.query.filter_by(id=current_user.marketplace_id).first()
+
+        if not marketplace:
+            flash("Marketplace não encontrado.", "danger")
+            return redirect(url_for('logout'))
+
+        # payload para enviar para a api da zoop, apenas dei um crtl c + ctrl v
+        payload = {
+            "first_name": first_name,
+            "last_name": last_name,
+            "email": email,
+            "phone_number": phone_number,
+            "taxpayer_id": taxpayer_id,
+            "birthdate": birthdate,
+            "statement_descriptor": statement_descriptor,
+            "revenue": revenue,
+            "address": address,
+            "mcc": mcc,
+        }
+
+        # chamando api da zoop conforme previsto na doc!
+        url = f"https://api.zoop.ws/v1/marketplaces/{marketplace.external_id}/sellers/individuals"
+        headers = {
+            "accept": "application/json",
+            "content-type": "application/json"
+        }
+        res = requests.post(url, json=payload, headers=headers, auth=(marketplace.private_key, None))
+
+        if res.status_code == 201:
+            flash("Pessoa Física criada com sucesso!", "success")
+            return redirect(url_for('transactions'))
+        else:
+            flash("Erro ao criar Pessoa Física: " + res.json().get("error", "Erro desconhecido"), "danger")
+            return redirect(url_for('pessoa_fisica'))  # Corrigido para redirecionar para a rota
+
+    # template de pessooafisica 
+    return render_template('theme/pessoafisica-register.html')
+
+
---
+++ app/models/pessoafisica.py
@@ -0,0 +1,29 @@
+from sqlalchemy import Column, Integer, String, Date, Float
+from sqlalchemy.ext.declarative import declarative_base
+
+Base = declarative_base()
+
+class PessoaFisica(Base):
+    __tablename__ = 'pessoa_fisica'  # Nome da tabela no banco de dados
+
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    first_name = Column(String(50), nullable=False)
+    last_name = Column(String(50), nullable=False)
+    email = Column(String(100), nullable=False, unique=True)
+    phone_number = Column(String(20), nullable=False)
+    taxpayer_id = Column(String(11), nullable=False)  # cpf 
+    birthdate = Column(Date, nullable=False)
+    statement_descriptor = Column(String(100), nullable=True)
+    revenue = Column(Float, nullable=False)  #float ou decimal para melhor precisao
+    address_line1 = Column(String(100), nullable=False)
+    address_line2 = Column(String(10), nullable=False)
+    address_line3 = Column(String(100), nullable=True)
+    neighborhood = Column(String(50), nullable=False)
+    city = Column(String(50), nullable=False)
+    state = Column(String(2), nullable=False)  # UF do user
+    postal_code = Column(String(10), nullable=False)  # CEP do user!
+    country_code = Column(String(2), default='BR', nullable=False)  # Código do país
+    mcc = Column(String(10), nullable=False)  # não entendi mt bem oq é mcc, apenas segui a doc lkkkkkkk
+
+    def __repr__(self):
+        return f"<PessoaFisica(id={self.id}, first_name='{self.first_name}', last_name='{self.last_name}')>"
